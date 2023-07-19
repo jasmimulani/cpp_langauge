898ff032775e479e40f6db456f202b4bc6f81d37
Jasmin <jasmimulani@gmail.com>
2023-07-19 18:50:48 +0530

    19-07-23

---
+++ ex_6.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class report
+{
+private:
+int adno,i;
+char name[20];
+float marks[5],average,sum=0;
+
+void getavg()
+{
+    for(i=0;i<5; i++)
+    {
+        sum+=marks[i];
+    }
+     average=sum/5;
+}
+
+public:
+
+void readinfo()
+{
+    cout<<"enter admin no:";
+    cin>>adno;
+    cout<<"enter name:";
+    cin>>name;
+    cout<<"enter 5 subject marks:";
+    for(i=0; i<5; i++)
+    {
+        cin>>marks[i];
+    }
+     getavg();
+}
+  void displayinfo()
+  {
+    cout<<"\n"<<"adno\t"<<"name\t"<<"adno\t"<<endl;
+    cout<<"\n"<<adno<<"\t"<<name<<"\t"<<adno<<"\t"<<endl;
+    for(i=0; i<5; i++)
+    {
+        cout<<marks[i]<<endl;
+    }
+     cout<<"average:"<<average<<endl;
+  }
+};
+  int main()
+  {
+    report m;
+    m.readinfo();
+    m.displayinfo();
+  }
