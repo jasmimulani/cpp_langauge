6a647f3b99476c865e4c48f7244d4478d47da049
Jasmin <jasmimulani@gmail.com>
2023-08-01 18:02:23 +0530

    1-08-23

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,3 +1,29 @@
 #include<iostream>
 using namespace std;
-class
+class base{
+    int a;
+    protected:
+    int l;
+    public:
+    void hello()
+    {
+        cout<<"hello worle...."<<endl;
+    }
+};
+ class test: public base
+ {
+    public:
+    void get()
+    {
+        base :: l=101;
+        cout<<"test class called...."<<endl;
+        cout<<"value of lis:"<<l<<endl;
+    }
+ };
+    int main()
+    {
+        test t;
+        t.hello();
+        t.get();
+
+    }
